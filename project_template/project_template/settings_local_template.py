6ba641fac970876468ca51b181c6efd7d6ee412d
Colorado Reed <colorado.j.reed@gmail.com>
2013-11-15 14:53:53 -0800

    initial basic django template

---
+++ project_template/project_template/settings_local_template.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'somestringofchars'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
